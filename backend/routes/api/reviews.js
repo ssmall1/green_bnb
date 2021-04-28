1c5861ec8d49608f7dc4e088015c7c8a4e720bb0
schuler <schuler.small.10@gmail.com>
2021-04-28 17:01:53 -0500

    built out reviews api route

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const asyncHandler = require('express-async-handler');
+const { User, Spot, Booking, Review } = require('../../db/models');
+
+router.get('/:id', asyncHandler(async function(req, res) {
+  const reviews = await Review.findAll({
+    where: {
+      spotId: req.params.id
+    }, 
+    include: { User },
+    order: [['updatedAt', 'DESC']]
+  });
+  return res.json(reviews);
+
+}))
+
+router.post('/', asyncHandler(async function (req, res) {
+    const newReview = await Review.create(req.body);
+    return res.json(newReview);
+  })
+);

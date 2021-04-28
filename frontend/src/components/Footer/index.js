cd33abee3a1f33a6d7b10c9add14ca2cdd8da29d
schuler <schuler.small.10@gmail.com>
2021-04-27 22:23:08 -0500

    added linkedin to footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,6 +6,14 @@ function Footer () {
         <>
             <div className="footer-container">
                 <div className="copyright">© 2021 GreenBnB</div>
+                <div className="linkedin">
+                    <a href="https://www.linkedin.com/in/schuler-small/" target="_blank">
+                        <div className="connect">
+                            <i class="fab fa-linkedin-in"></i>
+                             ~Connect with me
+                        </div>
+                    </a>
+                </div>
             </div>
         </>
     )

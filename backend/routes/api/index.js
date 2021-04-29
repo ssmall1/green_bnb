97667ebd4f6dfb6c6ec3832034e97d0a7e903560
schuler <schuler.small.10@gmail.com>
2021-04-29 12:15:04 -0500

    added review routes to spots

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,15 +2,12 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
-const reviewsRouter = require('./reviews.js');
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
 router.use('/spots', spotsRouter);
-
-router.use('/reviews', reviewsRouter);
 
 module.exports = router;
 
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -17,5 +17,31 @@ router.get('/:id', asyncHandler(async function(req, res) {
   return res.json(spot);
 }));
 
+router.get('/:id/reviews', asyncHandler(async (req, res) => {
+  const id = req.params.id
+  const reviews = await Review.findAll({where: {spotId: id}, include: User });
+
+  return res.json(reviews);
+}));
+
+router.post('/:id/post/review', asyncHandler(async (req, res) => {
+    const spotId = req.params.id;
+    const { authorId, rating, body } = req.body;
+
+    const review = await Review.build({
+      authorId: authorId,
+      spotId: spotId,
+      rating: rating,
+      body: body,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    await review.save();
+
+    const newReview = await Review.findOne({where: {id: review.id}, include: User});
+    return res.json(newReview);
+}));
+
 
 module.exports = router;

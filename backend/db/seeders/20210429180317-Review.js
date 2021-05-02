f57e47291490e7a68bf8c16c5e3753939cd6e342
schuler <schuler.small.10@gmail.com>
2021-05-02 16:49:29 -0500

    added reviews seeders

--- backend/db/seeders/20210429180317-Review.js
+++ backend/db/seeders/20210429180317-Review.js
@@ -5,18 +5,18 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Reviews', [
       {
-      authorId: 1,
+      authorId: 11,
       spotId: 2,
       rating: 5,
-      body: faker.random.words(),
+      body: "Great location and very clean!",
       createdAt: new Date(),
       updatedAt: new Date()
       },
       {
       authorId: 6,
       spotId: 2,
-      rating: 2,
-      body: faker.random.words(),
+      rating: 3,
+      body: "sufficient but was not impressed",
       createdAt: new Date(),
       updatedAt: new Date()
       },
@@ -24,18 +24,41 @@ module.exports = {
       authorId: 3,
       spotId: 2,
       rating: 4,
-      body: faker.random.words(),
+      body: "Such a cute and fun place",
       createdAt: new Date(),
       updatedAt: new Date()
       },
       {
-      authorId: 4,
-      spotId: 2,
-      rating: 5,
-      body: faker.random.words(),
-      createdAt: new Date(),
-      updatedAt: new Date()
+        authorId: 4,
+        spotId: 1,
+        rating: 5,
+        body: "Really quiet and tranquil",
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
+      {
+        authorId: 13,
+        spotId: 1,
+        rating: 5,
+        body: "Amazing! Will come back again!",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+        {
+        authorId: 6,
+        spotId: 5,
+        body: "Fully equipped and ready to entertain",
+        createdAt: new Date(),
+        updatedAt: new Date()
+        },
+        {
+        authorId: 14,
+        spotId: 3,
+        rating: 4,
+        body: "Such a cute and fun place",
+        createdAt: new Date(),
+        updatedAt: new Date()
+        },
   ], {});
   },
 

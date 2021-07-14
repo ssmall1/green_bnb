3a5d32b7aaa311ce08046cb6d3e96a10e8dcfd91
schuler <schuler.small.10@gmail.com>
2021-07-14 14:35:05 -0500

    styled host page

--- frontend/src/components/CreateSpot/index.js
+++ frontend/src/components/CreateSpot/index.js
@@ -61,102 +61,102 @@ const CreateSpot = () => {
   if (!spots) return null;
 
   return (
-    <>
-        <div className="host-spot-wrapper">
-            <h1>Host a Spot</h1>
-            {/* {errors.length > 0 &&
-                errors.map((error) => <div key={error}>{error}</div>)} */}
-            <form
-                style={{ display: "flex", flexFlow: "column" }}
-                onSubmit={handleSubmit}
-                id="create-spot-form"
-            >
-                <label>
-                <input
-                    type="text"
-                    placeholder="Title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-                </label>
-                <label>
-                <input
-                    type="text"
-                    placeholder="Price"
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}
-                />
-                </label>
-                <label>
-                <input
-                    type="text"
-                    placeholder="Eco Features"
-                    value={ecoFeatures}
-                    onChange={(e) => setEcoFeatures(e.target.value)}
-                />
-                </label>
-                <label>
-                <input
-                    type="text area"
-                    placeholder="Description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                />
-                </label>
-                <label>
-                <input
-                    type="text"
-                    placeholder="1234 N Adventure Ave"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                />
-                </label>
-                <label>
-                <input
-                    type="text"
-                    placeholder="City"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                />
-                </label>
-                <label>
-                <input
-                    type="text"
-                    placeholder="State"
-                    value={state}
-                    onChange={(e) => setState(e.target.value)}
-                />
-                </label>
-                <label>
-                <input
-                    type="text"
-                    placeholder="Zip Code"
-                    value={zip}
-                    onChange={(e) => setZip(e.target.value)}
-                />
-                </label>
-                <label>
-                <input
-                    type="text"
-                    placeholder="Country"
-                    value={country}
-                    onChange={(e) => setCountry(e.target.value)}
-                />
-                </label>
-                <label>
-                <input id="file-input" type="file" onChange={updateFile} />
-                </label>
-                {/* <label>
-                    Multiple Upload
-                    <input 
-                    type="file"
-                    multiple
-                    onChange={updateFiles} />
-                </label> */}
-                <button id="create-spot" type="submit">Create Spot</button>
-            </form>
-        </div>
-    </>
+    <div className="host-spot-wrapper">
+        {/* {errors.length > 0 &&
+            errors.map((error) => <div key={error}>{error}</div>)} */}
+        <form
+            style={{ display: "flex", flexFlow: "column" }}
+            onSubmit={handleSubmit}
+            id="create-spot-form"
+        >
+            <div id="host-title">Host a Spot</div>
+            <label>
+            <input
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            </label>
+            <label>
+            <input
+                type="text"
+                placeholder="Price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+            />
+            </label>
+            <label>
+            <input
+                type="text"
+                placeholder="Eco Features"
+                value={ecoFeatures}
+                onChange={(e) => setEcoFeatures(e.target.value)}
+            />
+            </label>
+            <label>
+            <input
+                type="text area"
+                placeholder="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
+            </label>
+            <label>
+            <input
+                type="text"
+                placeholder="1234 N Adventure Ave"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+            />
+            </label>
+            <label>
+            <input
+                type="text"
+                placeholder="City"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+            />
+            </label>
+            <label>
+            <input
+                type="text"
+                placeholder="State"
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+            />
+            </label>
+            <label>
+            <input
+                type="text"
+                placeholder="Zip Code"
+                value={zip}
+                onChange={(e) => setZip(e.target.value)}
+            />
+            </label>
+            <label>
+            <input
+                type="text"
+                placeholder="Country"
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+            />
+            </label>
+            <label>
+            <input id="file-input" type="file" onChange={updateFile} />
+            </label>
+            {/* <label>
+                Multiple Upload
+                <input 
+                type="file"
+                multiple
+                onChange={updateFiles} />
+            </label> */}
+            <button id="create-spot" type="submit">Create Spot</button>
+        </form>
+
+        <img src="https://greenbnb.s3.us-east-2.amazonaws.com/bigsky.jpg" alt="big sky"></img>
+    </div>
   );
 };
 
